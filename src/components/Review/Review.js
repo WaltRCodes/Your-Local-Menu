4117ed2fcb5c7a87bde6caad7ea56bc6e298dbf1
Visa <visalakshi.cse@gmail.com>
2020-05-15 17:45:19 -0500

    Review files added

---
+++ src/components/Review/Review.js
@@ -0,0 +1,23 @@
+import React,{Component} from './node_modules/react';
+import axios from './node_modules/axios';
+
+export default class Review extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+
+        return(
+<div>
+
+
+</div>
+
+        );
+    }
+}
