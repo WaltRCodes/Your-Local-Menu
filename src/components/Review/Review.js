caea9fa44e73e4de5c94e8d200311bceb80465a6
Visa <visalakshi.cse@gmail.com>
2020-05-17 09:53:51 -0500

    Formatted the code

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -7,11 +7,10 @@ export default class Review extends Component{
         super(props)
 
         this.state={
-userids:null,
-hdnBtn:true,
-value:'',
-txtid:0,
-id:-1
+            reviewData:null,
+            value:'',
+            txtid:0,
+            id:-1
         }
     }
     componentDidUpdate=()=>{
@@ -27,92 +26,76 @@ id:-1
             this.reviewRender();
         }
     }
-reviewRender=()=>{
-    
-       
-   console.log() 
-           
-           if(this.state.userids==null)
+    reviewRender=()=>{
+
+           if(this.state.reviewData==null)
            {
-           this.setState({userids:this.props.data})
-           
-   // console.log(this.state.userids[0].name)
-           }
-   
-   
-}
-handletextChange=(e)=>{
+           this.setState({reviewData:this.props.data})
+            }
+    }
+    handletextChange=(e)=>{
     this.setState({value:e.target.value})
     this.setState({txtid:e.target.id})
     console.log(this.state.value)
-}
-handleSubmit=(e)=>{
+    }
+    handleSubmit=(e)=>{
     e.preventDefault();
-    var temp=this.state.userids
+    var temp=this.state.reviewData
     temp[this.state.txtid].text=this.state.value;
-    this.setState({userids:temp})
+    this.setState({reviewData:temp})
     console.log(this.state.value)
-    this.setState({hdnBtn:true})
-}
-delete=(e)=>{
-console.log(e.target.id)
-var del=this.state.userids
-delete del[e.target.id]
-this.setState({userids:del})
-}
-edit=(e)=>{
-    e.preventDefault();
-    this.setState({id:e.target.id})
-    this.setState({hdnBtn:false})
+    this.setState({id:-1})
+    }
+    delete=(e)=>{
+    console.log(e.target.id)
+    var del=this.state.reviewData
+    delete del[e.target.id]
+    this.setState({reviewData:del})
+    }
+    edit=(e)=>{
+        e.preventDefault();
+        this.setState({id:e.target.id})
     
-}
+    }
     render(){
 
         return(
-<div className="container">
-<h1>Reviews </h1>
-<div className="flexparent">
-{
-this.state.userids!=null &&
-
-
-this.state.userids.map((dt,i)=>(
-<div>
-           <div className="flexchild">
-           <div className='thumbnaildiv'>
-           <img id="displaypic" src={dt.user.image_url}/>
-           <p>{dt.user.name}</p>
-           <div className="btn">
-           <button type="submit" id={i} onClick={this.edit}>Update</button>
-           <button type="submit" id={i} onClick={this.delete}>Delete</button>
-           
-           
-           </div>
-           </div>
-        
-           <div className="forflex">
-           Reviewed on {dt.time_created}<br />
-           {dt.rating} Stars  
-           <textarea id={i} defaultValue={dt.text} onChange={this.handletextChange} disabled={this.state.id==i ?false:true} / >
-         
-        </div>
-       
-           <br />
-         
-
-           </div>
-           <br />
-
-          </div>
-                ))
-}
+        <div className="container">
+        <h1>Reviews </h1>
+        <div className="flexparent">
+        {
+        this.state.reviewData!=null &&
+        this.state.reviewData.map((dt,i)=>(
+        <div>
+                <div className="flexchild">
+                <div className='thumbnaildiv'>
+                <img id="displaypic" src={dt.user.image_url}/>
+                <p>{dt.user.name}</p>
+                <div className="btn">
+                <button type="submit" id={i} onClick={this.edit}>Update</button>
+                <button type="submit" id={i} onClick={this.delete}>Delete</button>
+                </div>
+                </div>
                 
-<br />
-<br />
-</div>
+                <div className="forflex">
+                Reviewed on {dt.time_created}<br />
+                {dt.rating} Stars  
+                <textarea id={i} defaultValue={dt.text} onChange={this.handletextChange} disabled={this.state.id==i ?false:true} / >
+                </div>
+                <button type="submit" id={i} onClick={this.handleSubmit} hidden={this.state.id==i ?false:true}>Submit</button>
+                <br />
+                </div>
+                <br />
+                </div>
+                 ))
+        }
+                        
+        <br />
+        <br />
+        </div>
 
-</div>
+        </div>
 
-        );
+         );
     }
 }
