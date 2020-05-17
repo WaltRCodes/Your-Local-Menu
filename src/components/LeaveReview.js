f9ec17500ba776bd7007c40760c33ae4639d4ce0
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-05-16 23:24:40 -0400

    made the leae review component

---
+++ src/components/LeaveReview.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Call extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isFillingOutForm: false
+        }
+  }
+
+    
+  
+  render() {
+    return (
+      <div>
+        
+        
+      </div>
+      
+    )
+  }
+}
