d70eca95546ee4566732c6f74bb7e83605c7d202
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-05-16 23:35:38 -0400

    made the form for leaving a review

--- src/components/LeaveReview.js
+++ src/components/LeaveReview.js
@@ -14,8 +14,25 @@ export default class Call extends Component {
     return (
         this.state.isFillingOutForm ?
         <div>
-            <form>
-
+            <form onSubmit={props.submitReview}>
+                <label>
+                    Name:
+                    <input type="text" onChange={props.takeName} placeholder="Name"/>
+                    <br />
+                </label>
+                <label>
+                    Rating:
+                    <input type="number" onChange={props.takeRating} placeholder="0.0"/>
+                    <br />
+                </label>
+                <label>
+                    Comment:
+                    <input type="number" onChange={props.takeComment} placeholder="Comment"/>
+                    <br />
+                </label>
+                <label>
+                    <input type="submit" value="Submit"/>
+                </label>
             </form>
         </div>
       :
