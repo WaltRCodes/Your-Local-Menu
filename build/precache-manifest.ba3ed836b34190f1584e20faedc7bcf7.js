9f2ae56b63f4f41ae656409325aa057182bd8614
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-05-19 02:42:43 -0400

    deployed app and updated readme

---
+++ build/precache-manifest.ba3ed836b34190f1584e20faedc7bcf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f934cab5b531fdfecbca611282b01977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a2b6e1f20a4c6048be",
+    "url": "/static/css/main.07f83312.chunk.css"
+  },
+  {
+    "revision": "00213e141d493b72b613",
+    "url": "/static/js/2.d7d1f0d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7d1f0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a2b6e1f20a4c6048be",
+    "url": "/static/js/main.300d003c.chunk.js"
+  },
+  {
+    "revision": "8e6ab4fe8314fd08d762",
+    "url": "/static/js/runtime-main.0d69e76b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
